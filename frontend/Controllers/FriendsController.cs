1a78fc60fdbfd09b0966c7570c294b13f88aef08
“sebastiangrab73@gmail.comgit commit -m Stage <sebastiangrab73@gmail.com>
2023-12-19 20:28:43 +0100

    added frontend

---
+++ frontend/Controllers/FriendsController.cs
@@ -0,0 +1,108 @@
+﻿using FrontEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    public class FriendsController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiBaseUrl = "https://special-spork-v6v4wv7g9pr5cpr49-8080.app.github.dev/api/friends"; // Update with your API URL
+
+        public FriendsController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+
+        }
+
+        // GET: Friend
+        public async Task<IActionResult> Index()
+        {
+            var friends = await _httpClient.GetFromJsonAsync<List<FriendViewModel>>(_apiBaseUrl);
+            ViewBag.Message = TempData["Message"];
+            return View(friends);
+        }
+
+        // GET: Friend/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Friend/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Email,Phone,DoB")] FriendViewModel friend)
+        {
+            if (ModelState.IsValid)
+            {
+                await _httpClient.PostAsJsonAsync(_apiBaseUrl, friend);
+                TempData["Message"] = "Friend Created Successfully.!";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(friend);
+        }
+
+        // GET: Friend/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var friend = await _httpClient.GetFromJsonAsync<FriendViewModel>($"{_apiBaseUrl}/{id}");
+            if (friend == null)
+            {
+                return NotFound();
+            }
+            return View(friend);
+        }
+
+        // POST: Friend/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Email,Phone,DoB")] FriendViewModel friend)
+        {
+            if (id != friend.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _httpClient.PutAsJsonAsync($"{_apiBaseUrl}/{id}", friend);
+                 TempData["Message"] = "Friend Updated Successfully.!";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(friend);
+        }
+
+        // GET: Friend/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var friend = await _httpClient.GetFromJsonAsync<FriendViewModel>($"{_apiBaseUrl}/{id}");
+            if (friend == null)
+            {
+                return NotFound();
+            }
+
+            return View(friend);
+        }
+
+        // POST: Friend/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _httpClient.DeleteAsync($"{_apiBaseUrl}/{id}");
+             TempData["Message"] = "Friend Deleted Successfully.!";
+            return RedirectToAction(nameof(Index));
+        }
+    }
+    
+}
---
+++ frontend/Models/FriendViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FrontEnd.Models
+{
+    public class FriendViewModel
+    {
+         [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+
+        public string Name { get; set; }
+
+        [Column("email")]        
+        public string Email { get; set; }
+
+        [Column("phone")]
+        public string Phone { get; set; }
+
+        [Column("dob")]
+        [DisplayName("Date of Birth")]
+        public string DoB { get; set; }
+    }
+}
